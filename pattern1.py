b6c6305ac846e5a42012ef543a642303c5ced35d
JerinPaulS <jerinsprograms@gmail.com>
2024-04-20 12:37:01 +0530

    first pattern solved

---
+++ pattern1.py
@@ -0,0 +1,26 @@
+# *
+# **
+# ***
+# ****
+
+def pattern1(n):
+
+    for i in range(n + 1):
+        print("*" * i)
+
+def printSeparator():
+    print("---------------------------------------------------------------------------------------------------")
+
+pattern1(1)
+printSeparator()
+pattern1(2)
+printSeparator()
+pattern1(3)
+printSeparator()
+pattern1(4)
+printSeparator()
+pattern1(5)
+printSeparator()
+pattern1(10)
+printSeparator()
+    
